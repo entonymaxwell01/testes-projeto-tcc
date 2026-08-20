7026fecaf738a665fae1f04ec3efbf957843ca2a
Entony Maxwell da S. Oliveira <entony_oliveira@virgo.atlantico.net.br>
2026-08-20 10:17:32 -0300

    ci/cd: configurando relatorios do allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ import 'dotenv/config';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,7 +23,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+reporter: [
+    ['html', { open: 'never' }],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -52,10 +59,10 @@ export default defineConfig({
       dependencies: ['setup'], // Garante que o login ocorra antes
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     {
       name: 'webkit',
